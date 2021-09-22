dc6415951cffc90787c96a6ab56585c7b9d9533e
Will Crichton <wcrichto@cs.stanford.edu>
2021-09-22 15:08:42 -0700

    Fix tlist

--- src/tlist.rs
+++ src/tlist.rs
@@ -35,34 +35,12 @@ tyrade! {
     }
   }
 
-  // fn TListMap<L, F>() {
-  //   match L {
-  //     Nil => Nil,
-  //     Cons(X, XS) => Cons(F(X), TListMap(XS, F))
-  //   }
-  // }
-}
-
-pub trait ComputeTListMap<F> {
-  type Output;
-}
-pub type TListMap<L, F> = <L as ComputeTListMap<F>>::Output;
-pub struct ComputeTListMapFamily;
-impl Family2 for ComputeTListMapFamily {
-  type Func<L, F> = TListMap<L, F>;
-}
-impl<L, F> ComputeTListMap<F> for L {
-  default type Output = TyradeError;
-}
-impl<F> ComputeTListMap<F> for Nil {
-  type Output = Nil;
-}
-impl<XS, F, X> ComputeTListMap<F> for Cons<X, XS>
-where
-  XS: ComputeTListMap<F>,
-  F: Family1,
-{
-  type Output = Cons<F::Func<X>, TListMap<XS, F>>;
+  fn TListMap<L, F>() {
+    match L {
+      Nil => Nil,
+      Cons(X, XS) => Cons(F(X), TListMap(XS, F))
+    }
+  }
 }
 
 #[test]
@@ -72,5 +50,5 @@ fn list_test() {
 
   assert_type_eq::<S<S<Z>>, TListLen<Cons<(), Cons<(), Nil>>>>();
   assert_type_eq::<u64, TListNth<Cons<i32, Cons<u64, Nil>>, S<Z>>>();
-  assert_type_eq::<Cons<TFalse, Cons<TTrue, Nil>>, TListMap< Cons<S<Z>, Cons<Z, Nil>>, ComputeTIsZeroFamily>>();
+  assert_type_eq::<Cons<TFalse, Cons<TTrue, Nil>>, TListMap<Cons<S<Z>, Cons<Z, Nil>>, TIsZeroFamily>>();
 }
